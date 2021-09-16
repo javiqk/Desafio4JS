e3ba3e870b6ab64a1398a9b90caba5089e1cc8a2
javiqk <javiercucarella@hotmail.com>
2021-09-16 01:35:33 -0300

    se agregó texto

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ const iva = x => x * 0.21;
 let precioProducto = parseFloat(prompt("Ingrese el precio del producto"));
 let precioDescuento = 200
 let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), precioDescuento);
-console.log (nuevoPrecio);
+console.log ("El precio del producto con descuento es de " + nuevoPrecio + " (IVA incluido)");
